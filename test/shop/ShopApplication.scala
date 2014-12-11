c49242d10f0e19c5e6cf2616376be48119d66b5a
samutamm <samutamm@users.noreply.github.com>
2014-12-12 01:24:48 +0200

    Delete ShopApplication.scala

--- test/shop/ShopApplication.scala
+++
@@ -1,5 +0,0 @@
-package shop
-
-import play.api.test.{Helpers, FakeApplication, WithApplication}
-
-class ShopApplication extends WithApplication(FakeApplication(additionalConfiguration = Helpers.inMemoryDatabase()))

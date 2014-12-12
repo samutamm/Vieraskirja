2ba7425d51480ed482796be30366789fca5a4b98
Samu Tamminen <samutamm@dhcp-asv-96.helsinki.fi>
2014-12-12 20:55:17 +0200

    Added test that tests loading mainpage.

--- test/controllers/ViestiSpec.scala
+++ test/controllers/ViestiSpec.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import models.Vieraskirja
 import play.api.test.{PlaySpecification, FakeRequest}
 import play.api.libs.json.Json
 import vieraskirja.VieraskirjaApplication
@@ -11,12 +12,21 @@ class ViestiSpec extends PlaySpecification {
     val luoViesti = Json.obj("nimi" -> "API testausta", "sisalto" -> "jeeee")
     val luotuViesti = Json.obj("id" -> 1, "nimi" -> "API testausta", "sisalto" -> "jeeee")
 
-    //Tama feilaa enka ehdi nyt testata muita metodeita.. :/
-    "lisaa viesti JSON:illa" in new VieraskirjaApplication {
-      val response = call(Viestit.create, FakeRequest().withJsonBody(luoViesti))
-      status(response) must equalTo (OK)
-      contentAsJson(response) must equalTo (luotuViesti)
+    "lataa etusivu" in new VieraskirjaApplication {
+      val tulos = controllers.Application.index (FakeRequest())
+
+      status(tulos) must equalTo(OK)
+      contentType(tulos) must beSome("text/html")
     }
+
+
+    "lisaa viesti JSON:illa" in new VieraskirjaApplication {
+      val vastaus = call(Viestit.create, FakeRequest().withJsonBody(luoViesti))
+      status(vastaus) must equalTo (OK) //tutki tämä!
+      contentAsJson(vastaus) must equalTo (luotuViesti)
+    }
+    
+    
 
   }
 
